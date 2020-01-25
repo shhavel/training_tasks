8f60c66daf171ff2963810f0e168a86852cf5e37
Oleksandr Avoiants <shhavel@gmail.com>
2020-01-26 00:57:38 +0200

    Lesson #7 Task #2

---
+++ spec/07_stacks_and_queues_nesting_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+describe 'solution' do
+  def solution(s)
+    len = s.length
+    return 1 if len == 0
+    return 0 if len.odd?
+    opening_parenthesis = '('
+    closing_parenthesis = ')'
+    opened = 0
+    s.each_char do |parenthesis|
+      if parenthesis == opening_parenthesis
+        opened += 1
+      else
+        return 0 if opened == 0
+        opened -= 1
+      end
+    end
+    opened == 0 ? 1 : 0
+  end
+
+  it { expect(solution('(()(())())')).to eql(1) }
+  it { expect(solution('())')).to eql(0) }
+end
