a276401766e4340f1d171a5711ca866423d25080
Oleksandr Avoiants <shhavel@gmail.com>
2020-01-28 13:57:45 +0200

    L09T01 Combine cycles. Var naming.

--- spec/09_maximum_slice_problem_max_profit_spec.rb
+++ spec/09_maximum_slice_problem_max_profit_spec.rb
@@ -2,17 +2,14 @@
 
 describe 'solution' do
   def solution(a)
-    len = a.length
-    return 0 if len.zero?
-    a2 = Array.new(len, 0)
-    (1...len).each { |i| a2[i] = a[i] - a[i-1] }
-    cur_sum = 0 # current positive sum
-    max_sum = 0 # maximum sum so far
-    a2.each do |x|
-      max_sum = [max_sum, cur_sum].max
-      cur_sum = [cur_sum + x, 0].max
+    max_ending = 0 # max positive sum ending in current position
+    max_slice = 0
+    (1...a.length).each do |i|
+      diff = a[i] - a[i-1]
+      max_ending = [max_ending + diff, 0].max
+      max_slice = [max_slice, max_ending].max
     end
-    [cur_sum, max_sum, 0].max
+    max_slice
   end
 
   it { expect(solution([23171, 21011, 21123, 21366, 21013, 21367])).to eq(356) }
