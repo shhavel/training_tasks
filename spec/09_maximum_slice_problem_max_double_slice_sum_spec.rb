aa49b569f770efae6edafea7fd603470b210c8bd
Oleksandr Avoiants <shhavel@gmail.com>
2020-01-30 18:48:07 +0200

    L09T03 Var naming

--- spec/09_maximum_slice_problem_max_double_slice_sum_spec.rb
+++ spec/09_maximum_slice_problem_max_double_slice_sum_spec.rb
@@ -2,25 +2,24 @@
 
 describe 'solution' do
   # 0 ≤ X < Y < Z < N
-  # A[X+1] + A[X+2] + ... + A[Y−1] + A[Y+1] + A[Y+2] + ... + A[Z−1]
+  # A[X+1] + A[X+2] + ... + A[Y-1] + A[Y+1] + A[Y+2] + ... + A[Z-1]
   # Assume that if Y = X+1 then A[X+1] + ... + A[X] = 0
   def solution(a)
-    len = a.length
-    max_sum = max_left = max_right = 0
-    # 1 ≤ Y ≤ N-2, but we use an Array to store results, so start from 0.
-    max_lefts = (0..len-2).map do |y|
-      next 0 if y.zero? || y == 1
-      max_left = [max_left + a[y-1], 0].max
+    n = a.length
+    # 1 ≤ Y ≤ N-2, but we use Array to store results, so start from 0.
+    max_ending = Array.new(n-1, 0)
+    max_starting = Array.new(n-1, 0)
+    2.upto(n-2) do |y|
+      max_ending[y] = [max_ending[y-1] + a[y-1], 0].max
     end
-    max_rights = Array.new(len-1, 0)
-    (len-3).downto(1) do |y|
-      max_rights[y] = max_right = [a[y+1] + max_right, 0].max
+    (n-3).downto(1) do |y|
+      max_starting[y] = [a[y+1] + max_starting[y+1], 0].max
     end
-    (1..len-2).each do |y|
-      max_y_sum = max_lefts[y] + max_rights[y]
-      max_sum = [max_sum, max_y_sum].max
+    max_double_slice = 0
+    (1..n-2).each do |y|
+      max_double_slice = [max_double_slice, max_ending[y] + max_starting[y]].max
     end
-    max_sum
+    max_double_slice
   end
 
   it { expect(solution([3, 2, 6, -1, 4, 5, -1, 2])).to eq(17) }
